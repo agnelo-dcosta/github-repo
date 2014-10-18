4c75a6640ba90ffd7d7f7283bbfa12a1d48c9608
aggi.dcosta <agnelo.m.dcosta@gmail.com>
2014-10-18 13:04:30 -0400

    test

--- sql-data-reader/sql-query-data/src/org/aggi/sqldata/DatabaseReader.java
+++ sql-data-reader/sql-query-data/src/org/aggi/sqldata/DatabaseReader.java
@@ -1,5 +1,32 @@
 package org.aggi.sqldata;
 
+import java.sql.*;
+import java.util.List;
+
 public class DatabaseReader {
 
+	String fileName;
+	List <SqlQueryObject> queryList;
+	String database;
+	
+	public ResultSet readSql(Connection conn, String query) throws SQLException
+	{
+		Statement st;
+		ResultSet rs;
+		st = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY);
+		
+		rs = st.executeQuery(query);
+		
+		return rs;
+	}
+	
+	public void setUpReader(String fileName, String databaseName, List <SqlQueryObject> queryList)
+	{
+		
+	}
+	
+	public void replaceVariables()
+	{
+		
+	}
 }

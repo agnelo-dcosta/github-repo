6211c5996da033a4e3d58ddc2fca62f21b93e973
rsantos182003 <rsantos182003@yahoo.com>
2014-10-18 14:24:49 -0400

    checked in changes for data exporter

--- sql-data-reader/sql-query-data/test/org/aggi/sqldata/impl/DataExportImplTest.java
+++ sql-data-reader/sql-query-data/test/org/aggi/sqldata/impl/DataExportImplTest.java
@@ -2,6 +2,7 @@ package org.aggi.sqldata.impl;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,20 +20,27 @@ public class DataExportImplTest {
 	public void testExport() throws ServiceException {
 		dataExport = new DataExportImpl();
 		
+		DatabaseConn  dbconn= new DatabaseConn();
+		Connection conn = dbconn.getConnection("jdbc:oracle:thin:@EISORA001:1522:tiaa001", "AGGI2", "AGGI2_123");
+		Statement stmt;
 		try {
-			DatabaseConn  dbconn= new DatabaseConn();
-			Connection conn = dbconn.getConnection("jdbc:oracle:thin:@EISORA001:1522:tiaa001", "AGGI2", "AGGI2_123");
-			Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY);
-			 
-			ResultSet rs = stmt.executeQuery("select * from customer");
+			
 			Map<String, ResultSet> resultSets = new HashMap<String, ResultSet>();
+			
+			stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY);
+			ResultSet rs = stmt.executeQuery("select * from customer");			
 			resultSets.put("test1", rs);
+			
+			stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY);
+			rs = stmt.executeQuery("select * from employer");			
+			resultSets.put("test2", rs);
+			
 			dataExport.export("test-file", resultSets);
 			  
 			conn.close();
-		}
-		catch(Exception e) {
-			e.printStackTrace();
+		} catch (SQLException ex) {
+			// TODO Auto-generated catch block
+			throw new ServiceException(ex);
 		}
 	}
 
